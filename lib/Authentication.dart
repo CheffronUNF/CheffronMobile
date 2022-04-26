53af946a72beec1bb1c6e5d96f4b052cc3210c0c
Chase Wesley <chasewesley9451@gmail.com>
2022-04-26 17:20:31 -0400

    complete Authentication login()

--- lib/Authentication.dart
+++ lib/Authentication.dart
@@ -2,30 +2,25 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'main.dart';
 
-//*
-// Hailey F 4/26/22 -- Feel free to change, still learning
-//
-Future<String> createAuth(String username, String password) async {
+var _url = Uri.parse('https://elian.tk:8808/auth');
 
-  final response = await http.get(
-    Uri.parse('https://elian.tk:8808/auth'),
-      headers: {
-        'Authorization': base64.encode(utf8.encode(username + password))
-      },
-  );
+Future<String> login(String username, String password) async
+{
+  var encoded = base64.encode(utf8.encode(username + ":" + password));
+  var headers = { 'Authorization': "Basic " + encoded };
 
-  String jwt = response.body;
-  preferences.save("jwt", jwt);
+  final response = await http.get(_url, headers: headers);
 
   switch (response.statusCode)
   {
     case 200:
+      var body = jsonDecode(response.body);
+      String jwt = body["jwt"];
+      preferences.save("jwt", jwt);
       return "success";
     case 401:
       return "fail";
     default:
       return "malformed";
   }
-
-
 }
