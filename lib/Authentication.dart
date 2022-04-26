3ab10ef152f80f87b1b635734a83b2b438bdab17
Hailey Francis <n01402670@unf.edu>
2022-04-26 17:14:13 -0400

    update Authentication.dart (can prob be moved to AccountServices.dart)

--- lib/Authentication.dart
+++ lib/Authentication.dart
@@ -2,17 +2,20 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'main.dart';
 
-//Hailey F 4/26/22 -- Feel free to change, still learning
+//*
+// Hailey F 4/26/22 -- Feel free to change, still learning
+//
 Future<String> createAuth(String username, String password) async {
 
   final response = await http.get(
     Uri.parse('https://elian.tk:8808/auth'),
       headers: {
-        'Authorization': base64.encode(utf8.encode(username + password));
+        'Authorization': base64.encode(utf8.encode(username + password))
       },
   );
 
   String jwt = response.body;
+  preferences.save("jwt", jwt);
 
   switch (response.statusCode)
   {
