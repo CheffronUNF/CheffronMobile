6648980ada2b45d95c99f9752ece9d795ace650a
Hailey Francis <n01402670@unf.edu>
2022-04-26 17:01:50 -0400

    update Authentication.dart

--- lib/Authentication.dart
+++ lib/Authentication.dart
@@ -2,16 +2,17 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'main.dart';
 
-Future<String> createAuth(String username, String firstName, String lastName, String email, String password) async {
+//Hailey F 4/26/22 -- Feel free to change, still learning
+Future<String> createAuth(String username, String password) async {
+
   final response = await http.get(
     Uri.parse('https://elian.tk:8808/auth'),
       headers: {
-        'Authorization': username + password;
+        'Authorization': base64.encode(utf8.encode(username + password));
       },
-
-
-
   );
+
+  String jwt = response.body;
 
   switch (response.statusCode)
   {
@@ -22,4 +23,6 @@ Future<String> createAuth(String username, String firstName, String lastName, St
     default:
       return "malformed";
   }
+
+
 }
