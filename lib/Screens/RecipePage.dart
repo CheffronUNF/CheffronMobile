a870120641131886e7aba7c8956eee975ad904d9
Chase Wesley <chasewesley9451@gmail.com>
2022-04-30 17:25:04 -0400

    Hide yellow background behind recipe information on RecipePage

--- lib/Screens/RecipePage.dart
+++ lib/Screens/RecipePage.dart
@@ -23,15 +23,16 @@ class _RecipePageState extends State<RecipePage> {
   Widget build(BuildContext context) => _buildScaffold();
 
   _buildScaffold() => Scaffold(
+    backgroundColor: Colors.white,
     body: _buildContentContainer(),
   );
 
-  _buildContentContainer() => Container(
+  _buildContentContainer() => SizedBox(
     width: double.infinity,
     height: double.infinity,
     child: Stack(
       children: <Widget>[
-        Container(color: yellow), ///Image can go here
+        Container(height: 200, color: yellow), ///Image can go here
         _buildScrollSheet()
       ],
     ),
@@ -39,72 +40,75 @@ class _RecipePageState extends State<RecipePage> {
 
   _buildScrollSheet() => DraggableScrollableSheet(
     maxChildSize: 1,
-    initialChildSize: 0.9, ///lower if we have images
-    minChildSize: 0.9,
+    initialChildSize: 0.8, ///lower if we have images
+    minChildSize: 0.8,
     builder: (context, controller) => _buildScrollView(controller),
   );
   
   _buildScrollView(ScrollController controller) => SingleChildScrollView(
     controller: controller,
-    child: Container(
-      padding: const EdgeInsets.all(24),
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(40),
-          topRight: Radius.circular(40))),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text(
-                widget.recipe.recipeName,
-                style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-              ),
-              const Spacer(),
-            ],
-          ),
-          Text(
-            "By ${widget.owner != null ? widget.owner!.username : "unknown"}",
-            style: const TextStyle(fontWeight: FontWeight.w300),
-          ),
-          const SizedBox(height: 4),
-          const SizedBox(height: 24),
+    child: _buildScrollContent(),
+  );
 
-          ///Container for food information
-          Container(
-            child: Row(
-              children: <Widget>[
-                Expanded(child: _buildFoodInfoBox("Serves", widget.recipe.servings.toString())),
-                const SizedBox(width: 10),
-                const SizedBox(width: 10),
-                Expanded(child: _buildFoodInfoBox("Time", widget.recipe.time)),
-              ],
-            ),
-          ),
+  _buildScrollContent() => Container(
+    padding: const EdgeInsets.all(24),
+    decoration: const BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(40),
+        topRight: Radius.circular(40))),
+    child: _buildContentColumn(),
+  );
 
-          const SizedBox(height: 24),
-          _buildIngredientsHeader(),
+  _buildContentColumn() => Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      _buildRecipeNameHeader(),
+      _buildOwnerHeader(),
 
-          const SizedBox(height: 4),
-          _buildIngredientsList(),
+      const SizedBox(height: 28),
+      _buildFoodInfoContainer(),
 
-          const SizedBox(height: 24),
-          _buildDirectionsHeader(),
+      const SizedBox(height: 24),
+      _buildIngredientsHeader(),
 
-          const SizedBox(height: 4),
-          _buildDirectionsBox(),
-        ],
-      ),
+      const SizedBox(height: 4),
+      _buildIngredientsList(),
+
+      const SizedBox(height: 24),
+      _buildDirectionsHeader(),
+
+      const SizedBox(height: 4),
+      _buildDirectionsBox(),
+    ],
+  );
+
+  _buildRecipeNameHeader() => Text(
+    widget.recipe.recipeName,
+    style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+  );
+
+  _buildOwnerHeader() => Text(
+    "By ${widget.owner != null ? widget.owner!.username : "unknown"}",
+    style: const TextStyle(fontWeight: FontWeight.w300),
+  );
+
+  _buildFoodInfoContainer() => Container(
+    child: Row(
+      children: <Widget>[
+        Expanded(child: _buildFoodInfoBox("Serves", widget.recipe.servings.toString())),
+        const SizedBox(width: 10),
+        const SizedBox(width: 10),
+        Expanded(child: _buildFoodInfoBox("Time", widget.recipe.time)),
+      ],
     ),
   );
 
   _buildFoodInfoBox(String title, String value) => Container(
     decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        border:
-        Border.all(color: Colors.grey)),
+      borderRadius: BorderRadius.circular(10),
+      border:
+      Border.all(color: Colors.grey)),
     padding: const EdgeInsets.symmetric(vertical: 16),
     child: Column(
       children: <Widget>[
@@ -146,7 +150,7 @@ _getIngredients(Recipe recipe)
 
   for (var ingredient in recipe.ingredients)
   {
-    ingredients += '${ingredient.quantity} ${ingredient.unit} ${ingredient.name}';
+    ingredients += '${ingredient.quantity} ${ingredient.unit} ${ingredient.name}\n';
   }
 
   return ingredients;
